ff7b6372f636a9ee3b02b2f4a8864ad8ade02858
PiotrO15 <72670117+PiotrO15@users.noreply.github.com>
2026-04-25 13:09:53 +0200

    Added animated particle path for essence collector

--- src/main/java/me/verdo/elements/system/HarvestCropEventSystem.java
+++ src/main/java/me/verdo/elements/system/HarvestCropEventSystem.java
@@ -9,12 +9,14 @@ import com.hypixel.hytale.component.spatial.SpatialResource;
 import com.hypixel.hytale.component.system.EntityEventSystem;
 import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.server.core.HytaleServer;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.ecs.BreakBlockEvent;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.modules.block.BlockModule;
 import com.hypixel.hytale.server.core.modules.block.components.ItemContainerBlock;
+import com.hypixel.hytale.server.core.modules.entity.EntityModule;
 import com.hypixel.hytale.server.core.universe.world.ParticleUtil;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
@@ -25,6 +27,7 @@ import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class HarvestCropEventSystem extends EntityEventSystem<EntityStore, BreakBlockEvent> {
     public HarvestCropEventSystem(@NonNullDecl Class<BreakBlockEvent> eventType) {
@@ -45,38 +48,54 @@ public class HarvestCropEventSystem extends EntityEventSystem<EntityStore, Break
         List<Ref<ChunkStore>> results = SpatialResource.getThreadLocalReferenceList();
         blockStateSpatialStructure.getSpatialStructure().ordered3DAxis(event.getTargetBlock().toVector3d(), 10, 10, 10, results);
 
-        System.out.println("B");
-
         if (!results.isEmpty()) {
             for (Ref<ChunkStore> result : results) {
                 BlockModule.BlockStateInfo blockStateInfoI = world.getChunkStore().getStore().getComponent(result, BlockModule.BlockStateInfo.getComponentType());
+                if (blockStateInfoI == null) return;
                 Vector3i blockPosI = ModChunkUtil.getBlockPosFromIndex(blockStateInfoI);
 
                 Vector3d from = event.getTargetBlock().toVector3d().add(0.5, 0.5, 0.5);
                 Vector3d to = blockPosI.clone().toVector3d().add(0.5, 0.5, 0.5);
 
                 double distance = from.distanceTo(to);
-                int steps = (int) (distance / 0.5); // one particle every 0.5 blocks
+                int steps = (int) (distance / 0.5);
 
-                ItemContainerBlock containerBlock = chunkStore.getComponent(result, ItemContainerBlock.getComponentType());
-                if (containerBlock != null) {
-                    if (containerBlock.getItemContainer().addItemStack(new ItemStack("Ingredient_Life_Essence")).succeeded()) {
-                        System.out.println("!!!");
-                    }
-                }
+//                ItemContainerBlock containerBlock = chunkStore.getComponent(result, ItemContainerBlock.getComponentType());
+//                if (containerBlock != null) {
+//                    if (containerBlock.getItemContainer().addItemStack(new ItemStack("Ingredient_Life_Essence")).succeeded()) {
+//                    }
+//                }
 
-                for (int step = 0; step <= steps; step++) {
-                    double t = steps == 0 ? 0 : (double) step / steps;
+                double dx = to.x - from.x;
+                double dy = to.y - from.y;
+                double dz = to.z - from.z;
+
+                float yaw   = (float) Math.atan2(-dx, -dz);
+                float pitch = (float) Math.atan2(dy, Math.sqrt(dx * dx + dz * dz));
+
+                for (int step = 0; step <= steps - 1; step++) {
+                    double t = (double) step / steps;
                     Vector3d point = new Vector3d(
-                            from.x + (to.x - from.x) * t,
-                            from.y + (to.y - from.y) * t,
-                            from.z + (to.z - from.z) * t
+                            from.x + dx * t,
+                            from.y + dy * t,
+                            from.z + dz * t
                     );
 
-                    ParticleUtil.spawnParticleEffect("GreenOrbImpact", point, world.getEntityStore().getStore());
+                    HytaleServer.SCHEDULED_EXECUTOR.schedule(() -> world.execute(() -> {
+                        SpatialResource<Ref<EntityStore>, EntityStore> playerSpatialResource = world.getEntityStore().getStore().getResource(EntityModule.get().getPlayerSpatialResourceType());
+                        List<Ref<EntityStore>> playerRefs = SpatialResource.getThreadLocalReferenceList();
+                        playerSpatialResource.getSpatialStructure().collect(point, 75.0F, playerRefs);
+                        ParticleUtil.spawnParticleEffect("Essence_Collector_Transfer", point, yaw, pitch, 0f, null, playerRefs, world.getEntityStore().getStore());
+                    }), step * 100L, TimeUnit.MILLISECONDS);
                 }
 
-                System.out.println("Found block at " + blockPosI);
+                HytaleServer.SCHEDULED_EXECUTOR.schedule(() -> world.execute(() -> {
+                    ItemContainerBlock scheduledContainer = chunkStore.getComponent(result, ItemContainerBlock.getComponentType());
+                    if (scheduledContainer == null) return;
+
+                    if (scheduledContainer.getItemContainer().addItemStack(new ItemStack("Ingredient_Life_Essence")).succeeded()) {
+                    }
+                }), steps * 100L, TimeUnit.MILLISECONDS);
             }
         }
     }
